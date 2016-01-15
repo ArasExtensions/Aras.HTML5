4e3c665cbb355933f70ba412ed7b43b1b2e3cebe
Richard Mason <richard.mason@processwall.com>
2016-01-15 09:04:46 +0000

    Completed List

--- src/js/Aras/View/Properties/List.js
+++ src/js/Aras/View/Properties/List.js
@@ -25,14 +25,16 @@
 define([
 	'dojo/_base/declare',
 	'dojo/_base/lang',
+	'dojo/_base/array',
 	'../Property',
 	'dijit/form/Select'
-], function(declare, lang, Property, Select) {
+], function(declare, lang, array, Property, Select) {
 	
 	return declare('Aras.View.Properties.List', [Select, Property], {
-								
-		constructor: function() {
 		
+		_viewModelValueLoadedHandles : null,
+		
+		constructor: function() {
 			
 		},
 		
@@ -44,7 +46,95 @@ define([
 		OnViewModelLoaded: function() {
 			this.inherited(arguments);
 
+			// Remove existing watch handles
 			
+			if (this._viewModelValueLoadedHandles != null)
+			{
+				array.forEach(this._viewModelValueLoadedHandles, function(watchhandle, i) {
+					watchhandle.unwatch();
+				}, this);
+				
+				this._viewModelValueLoadedHandles.destroy();
+			}
+			
+			if (this.ViewModel != null)
+			{
+				// Check if values are loaded
+				var allloaded = true;
+				this._viewModelValueLoadedHandles = [];
+				
+				array.forEach(this.ViewModel.Values, function(valueviewmodel, i) {
+					
+					if (!valueviewmodel.Loaded)
+					{
+						allloaded = false;
+						
+						this._viewModelValueLoadedHandles[i] = valueviewmodel.watch('Loaded', lang.hitch(this, function(name, oldValue, newValue) {
+					
+							if (newValue)
+							{
+								// Add Options
+								this._addOptions();
+							}
+					
+						}));
+					}
+					
+				}, this);
+
+				if (allloaded)
+				{
+					// Add Options
+					this._addOptions();
+				}
+			}
+		},
+		
+		_addOptions: function()
+		{
+			// Load List Values if all loaded
+			var allloaded = true;
+			var options = [];
+			
+			array.forEach(this.ViewModel.Values, function(valueviewmodel, i) {
+			
+				if (valueviewmodel.Loaded)
+				{
+					if (this.ViewModel.Value == valueviewmodel.Value)
+					{
+						options[i] = { label: valueviewmodel.Label, value: valueviewmodel.Value, selected: true };
+					}
+					else
+					{
+						options[i] = { label: valueviewmodel.Label, value: valueviewmodel.Value };
+					}
+				}
+				else
+				{
+					allloaded = false;
+					options[i] = null;
+				}
+
+			}, this);
+			
+			if (allloaded)
+			{				
+				// Add Options
+				this.addOption(options);
+				
+				// Watch for change in Select Value
+				this.watch('value', lang.hitch(this, function(name, oldValue, newValue) {
+					
+					if (newValue !== this.ViewModel.Value)
+					{
+						this.ViewModel.Value = newValue;
+						this.ViewModel.Write();
+					}
+					
+				}));				
+				
+
+			}
 		}
 
 	});
