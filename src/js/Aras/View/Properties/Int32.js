13290606710d717ec5f926dabbc7f41b0add1f90
Richard Mason <richard.mason@processwall.com>
2016-02-22 11:57:43 +0000

    Added Paging to Search

---
+++ src/js/Aras/View/Properties/Int32.js
@@ -0,0 +1,92 @@
+/*
+  Aras.HTML5 provides a HTML5 client library to build Aras Innovator Applications
+
+  Copyright (C) 2015 Processwall Limited.
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as published
+  by the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see http://opensource.org/licenses/AGPL-3.0.
+ 
+  Company: Processwall Limited
+  Address: The Winnowing House, Mill Lane, Askham Richard, York, YO23 3NW, United Kingdom
+  Tel:     +44 113 815 3440
+  Email:   support@processwall.com
+*/
+
+define([
+	'dojo/_base/declare',
+	'dojo/_base/lang',
+	'dojo/on',
+	'../Property',
+	'dijit/form/NumberSpinner'
+], function(declare, lang, on, Property, NumberSpinner) {
+	
+	return declare('Aras.View.Properties.Int32', [NumberSpinner, Property], {
+		
+		_viewModelValueHandle: null,
+		
+		constructor: function() {
+
+		},
+		
+		startup: function() {
+			this.inherited(arguments);			
+		},
+		
+		OnViewModelLoaded: function() {
+			this.inherited(arguments);
+
+			// Set Value
+			this.set("value", this.ViewModel.Value);
+			
+			// Watch for changes in Control value
+			this.watch('value', lang.hitch(this, function(name, oldValue, newValue) {
+				
+				if (isNaN(newValue))
+				{
+					this.set("value", oldValue);
+				}
+				else
+				{
+					var newnumber = Number(newValue);
+					var oldnumber = Number(oldValue);
+				
+					if (oldnumber !== newnumber)
+					{						
+						// Update ViewModel Value
+						this.ViewModel.set('Value', newnumber);
+						this.set("value", newnumber);
+						this.ViewModel.Write();
+					}
+				}
+					
+			}));
+			
+			// Watch for changes in ViewModel
+			if (this._viewModelValueHandle != null)
+			{
+				this._viewModelValueHandle.unwatch();
+			}
+			
+			this._viewModelValueHandle = this.ViewModel.watch('Value', lang.hitch(this, function(name, oldValue, newValue) {
+					
+				if (newValue)
+				{
+					// Set Value
+					this.set("value", this.ViewModel.Value);
+				}
+					
+			}));
+		}
+
+	});
+});
--- src/js/Aras/View/Search.js
+++ src/js/Aras/View/Search.js
@@ -27,17 +27,25 @@ define([
 	'dojo/_base/lang',
 	'dijit/layout/BorderContainer',
 	'dijit/layout/ContentPane',
+	'dijit/ToolbarSeparator',
+	'dijit/form/NumberSpinner',
 	'./Control',
 	'./Toolbar',
 	'./Button',
 	'./Grid'
-], function(declare, lang, BorderContainer, ContentPane, Control, Toolbar, Button, Grid) {
+], function(declare, lang, BorderContainer, ContentPane, ToolbarSeparator, NumberSpinner, Control, Toolbar, Button, Grid) {
 	
 	return declare('Aras.View.Search', [BorderContainer, Control], {
 	
 		Toolbar: null,
 		
 		SearchButton: null,
+		
+		PageSize: null,
+		
+		NextButton: null,
+		
+		PreviousButton: null,
 				
 		Grid: null,
 		
@@ -55,6 +63,24 @@ define([
 			// Create Search Button
 			this.SearchButton = new Button({ iconClass: 'searchIcon'});
 			this.Toolbar.addChild(this.SearchButton);
+	
+			// Add Separator
+			this.Toolbar.addChild(new ToolbarSeparator());
+			
+			// Create Page Size
+			this.PageSize = new NumberSpinner({ value:25, constraints: { min:5, max:100, places:0 }, style: 'width:40px; margin-left:5px;'});
+			this.Toolbar.addChild(this.PageSize);
+			
+			// Create Next Page Button
+			this.NextButton = new Button({ iconClass: 'nextPageIcon'});
+			this.Toolbar.addChild(this.NextButton);
+		
+			// Create Previous Page Button
+			this.PreviousButton = new Button({ iconClass: 'previousPageIcon'});
+			this.Toolbar.addChild(this.PreviousButton);
+			
+			// Add Separator
+			this.Toolbar.addChild(new ToolbarSeparator());
 			
 			// Create Grid			
 			this.Grid = new Grid({style: 'height: 100%; width: 100%', region: 'center', gutters: false });		
@@ -71,6 +97,30 @@ define([
 				
 				// Set Search Button ViewModel			
 				this.SearchButton.set("ViewModel", this.ViewModel.Refresh);	
+				
+				// Set Page Size
+				this.PageSize.set("value", this.ViewModel.PageSize);
+
+				// Watch for changes in PageSize
+				this.PageSize.watch("value", lang.hitch(this, function(name, oldValue, newValue) {
+									
+					if (isNaN(newValue))
+					{
+						this.PageSize.set("value", oldValue);
+					}
+					else
+					{
+						var newnumber = Number(newValue);
+						var oldnumber = Number(oldValue);
+				
+						if (oldnumber !== newnumber)
+						{						
+							// Update ViewModel Value
+							this.ViewModel.set('PageSize', newnumber);
+							this.ViewModel.Write();
+						}
+					}
+				}));
 			}
 		}
 	});
