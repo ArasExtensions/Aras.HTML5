110b1a6eb2dc630c7b26a33b050b2b603c870a54
Richard Mason <richard.mason@aessis.com>
2016-03-02 15:16:20 +0000

    Added PartRelationshipTree Plugin

---
+++ src/js/Aras/View/Design/Plugins/PartRelationshipTree.js
@@ -0,0 +1,43 @@
+/*
+  Aras.HTML5 provides a HTML5 client library to build Aras Innovator Applications
+
+  Copyright (C) 2015 Processwall Limited.
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as published
+  by the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see http://opensource.org/licenses/AGPL-3.0.
+ 
+  Company: Processwall Limited
+  Address: The Winnowing House, Mill Lane, Askham Richard, York, YO23 3NW, United Kingdom
+  Tel:     +44 113 815 3440
+  Email:   support@processwall.com
+*/
+
+define([
+	'dojo/_base/declare',
+	'dojo/_base/lang',
+	'dojo/when',
+	'../../Plugin',
+	'../../RelationshipTree'
+], function(declare, lang, when, Plugin, RelationshipTreeControl) {
+	
+	return declare('Aras.View.Design.Plugins.RelationshipTree', [Plugin, RelationshipTreeControl], {
+			
+		startup: function() {
+
+			this.inherited(arguments);
+			
+			// Initialise
+			this.Initialise();
+		}
+	});
+});
